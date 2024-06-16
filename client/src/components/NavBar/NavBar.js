68a466ca5b33dab7af1bec2ced9dabc97b4e9db7
Crackvignoule <kiki.kalagan@gmail.com>
2024-06-16 14:49:51 +0200

    adding setting cog

--- client/src/components/NavBar/NavBar.js
+++ client/src/components/NavBar/NavBar.js
@@ -1,18 +1,22 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCog } from '@fortawesome/free-solid-svg-icons';
 import './NavBar.scss';
 
 function NavBar() {
   return (
     <nav className="navbar">
-      <h1 className="navbar__title">My Dashboard</h1>
+      <NavLink className="navbar__title" to="/home">
+        <h1>Debrid Client</h1>
+      </NavLink>
       <div className="navbar__links">
-        <NavLink className="navbar__link" to="/page1">Page 1</NavLink>
+        <NavLink className="navbar__link" to="/home">Home</NavLink>
         <NavLink className="navbar__link" to="/page2">Page 2</NavLink>
       </div>
-      <div className="navbar__settings">
-        <i className="fas fa-cog"></i>
-      </div>
+      <NavLink className="navbar__settings" to="/settings">
+        <FontAwesomeIcon icon={faCog} />
+      </NavLink>
     </nav>
   );
 }
