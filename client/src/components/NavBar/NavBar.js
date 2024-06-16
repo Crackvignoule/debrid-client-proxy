391926e6210e6c8a3c15394c209506a7257123a0
Crackvignoule <kiki.kalagan@gmail.com>
2024-06-16 15:39:56 +0200

    WIP settings

--- client/src/components/NavBar/NavBar.js
+++ client/src/components/NavBar/NavBar.js
@@ -1,4 +1,3 @@
-import React from 'react';
 import { NavLink } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCog } from '@fortawesome/free-solid-svg-icons';
--- client/src/pages/Settings/Settings.js
+++ client/src/pages/Settings/Settings.js
@@ -1,8 +1,16 @@
+import { useState } from 'react';
+
 function Settings() {
+  const [apiKey, setApiKey] = useState('');
+
+  const handleInputChange = (event) => {
+    setApiKey(event.target.value);
+  };
+
   return (
     <div>
       <h1>Settings</h1>
-      {/* <p>This is the home page of our application.</p> */}
+      
     </div>
   );
 }
