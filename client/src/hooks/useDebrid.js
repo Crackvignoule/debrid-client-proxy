6ddd89a71f7b8db437c74c3b2979bb79b5140283
Crackvignoule <88386090+Crackvignoule@users.noreply.github.com>
2024-06-20 08:50:30 +0200

    Update useDebrid.js

--- client/src/hooks/useDebrid.js
+++ client/src/hooks/useDebrid.js
@@ -3,58 +3,43 @@ import axios from "axios";
 
 export function useDebrid() {
   const [debridResult, setDebridResult] = useState(null);
-  // TODO Simplify this big bullsh*t but keep it working
+  // TODO Add promise toast
+  // TODO When debrid will take normal links, I should review the whole blocking input logic and debrid would take magnetOrTorrentOrLinks
 
-  const debrid = (magnetLinkOrFile) => {
-    if (typeof magnetLinkOrFile === 'string') {
-      // Handle magnet link
-      getMagnetID(magnetLinkOrFile);
-    } else {
-      // Handle file
-      // TODO Keep debrid func simple: getMagnetID => debridMagnet
-      const formData = new FormData();
-      formData.append('file', magnetLinkOrFile);
-
-      axios
-        .post("/api/debrid/getMagnetID", formData, {
-          headers: {
-            'Content-Type': 'multipart/form-data'
-          }
-        })
-        .then((response) => {
-          debridMagnet(response.data.magnetID);
-        })
-        .catch((error) => {
-          console.error("Error getting magnet ID: ", error);
-        });
+  const debrid = async (magnetLinkOrFile) => {
+    try {
+      const magnetID = await getMagnetID(magnetLinkOrFile);
+      const result = await debridMagnet(magnetID);
+      setDebridResult(result);
+    } catch (error) {
+      console.error("Error in debrid process: ", error);
     }
   };
 
   const getMagnetID = async (input) => {
     const apiKey = localStorage.getItem("apiKey");
     let data;
-    let headers;
+    let headers = { 'api-key': apiKey };
 
     if (typeof input === 'string') {
       // Handle magnet link
       data = { magnetLink: input };
-      headers = { 'api-key': apiKey };
     } else {
       // Handle file
       data = new FormData();
       data.append('torrent', input);
-      headers = { 'api-key': apiKey };
     }
 
     try {
       const response = await axios.post('/api/debrid/getMagnetID', data, { headers });
       console.log("Magnet ID received: ", response.data.id);
-      debridMagnet(response.data.id);
+      return response.data.id;
     } catch (error) {
       console.error("Error getting magnet ID: ", error);
+      throw error;
     }
   };
-
+  
   const debridMagnet = async (magnetID) => {
     try {
       const response = await axios.post("/api/debrid/debridMagnet", { magnetID });
