057aa7982cd285ce67a944f5c13834bcc4e9ab2d
Crackvignoule <88386090+Crackvignoule@users.noreply.github.com>
2024-06-25 11:59:07 +0200

    Update api.js

--- server/routes/api.js
+++ server/routes/api.js
@@ -3,15 +3,8 @@ const router = express.Router();
 
 const debridRoutes = require('./debridRoutes');
 const checkApiKeyRoutes = require('./checkApiKeyRoutes');
-// const uploadRoutes = require('./uploadRoutes');
-// const debridLinksRoutes = require('./debridLinksRoutes');
-// const debridMagnetRoutes = require('./debridMagnetRoutes');
-// const debridTorrentRoutes = require('./debridTorrentRoutes');
 
 router.use('/debrid', debridRoutes);
 router.use('/checkApiKey', checkApiKeyRoutes);
-// router.use('/debridLinks', debridLinksRoutes);
-// router.use('/debridMagnet', debridMagnetRoutes);
-// router.use('/debridTorrent', debridTorrentRoutes);
 
 module.exports = router;
