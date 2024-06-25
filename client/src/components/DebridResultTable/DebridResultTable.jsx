6df4e7bd2f2955a93f0188d926066d7d214c7a39
Crackvignoule <kiki.kalagan@gmail.com>
2024-06-25 21:19:19 +0200

    added copy button

--- client/src/components/DebridResultTable/DebridResultTable.jsx
+++ client/src/components/DebridResultTable/DebridResultTable.jsx
@@ -1,9 +1,10 @@
-// TODO Fix: You could also use Array.map to render the items, but it will not be as performant as using the items and columns prop.
+// TODO Refactor
 import React from 'react';
 import { Table, TableHeader, TableColumn, TableBody, TableRow, TableCell, getKeyValue } from "@nextui-org/react";
 import { Button } from "@nextui-org/react";
 import { Save, SaveAll, Download, Copy } from 'lucide-react';
 import { useSaveLinks } from '../../hooks';
+import { toast } from 'react-hot-toast';
 
 function DebridResultTable({ debridResult }) {
   const { saveLinks } = useSaveLinks();
@@ -13,11 +14,18 @@ function DebridResultTable({ debridResult }) {
     saveLinks(allLinks);
   };
 
+  // Function to copy link to clipboard
+  const copyToClipboard = (link) => {
+    navigator.clipboard.writeText(link).then(() => {
+      toast.success('Link copied to clipboard 👌');
+    })
+  };
+
   const columns = [
     { key: "no", label: "No." },
     { key: "filename", label: "Filename" },
     { key: "debridedLink", label: "Debrided Link" },
-    { key: "action", label: "Action" },
+    { key: "actions", label: "Actions" },
   ];
 
   return (
@@ -50,20 +58,34 @@ function DebridResultTable({ debridResult }) {
                 <TableRow key={item.no}>
                   {(columnKey) => (
                     <TableCell>
-                      {columnKey === "action" ? (
-                        <Button
-                          isIconOnly
-                          onClick={() => saveLinks([item.link])}
-                          className="bg-cadet-grey"
-                        >
-                          <Save />
-                        </Button>
+                      {columnKey === "actions" ? (
+                        <div className="flex items-center gap-2.5">
+                          <Button
+                            isIconOnly
+                            onClick={() => saveLinks([item.link])}
+                            className="bg-cadet-grey"
+                          >
+                            <Save />
+                          </Button>
+                          <Button
+                            isIconOnly
+                            onClick={() => copyToClipboard(item.link)}
+                            className="bg-cadet-grey"
+                          >
+                            <Copy />
+                          </Button>
+                        </div>
                       ) : columnKey === "debridedLink" ? (
-                        <a href={getKeyValue(item, columnKey)}>
-                          {getKeyValue(item, columnKey)}
+                        <a
+                          href={item.debridedLink}
+                          target="_blank"
+                          rel="noopener noreferrer"
+                          className="block truncate max-w-xs"
+                        >
+                          {item.debridedLink}
                         </a>
                       ) : (
-                        getKeyValue(item, columnKey)
+                        item[columnKey]
                       )}
                     </TableCell>
                   )}
