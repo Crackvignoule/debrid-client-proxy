119703f1cac828675da497da81771634dc955a12
Crackvignoule <kiki.kalagan@gmail.com>
2024-06-26 08:17:34 +0200

    WIP SavedLinks

--- client/src/pages/SavedLinks/SavedLinks.jsx
+++ client/src/pages/SavedLinks/SavedLinks.jsx
@@ -3,34 +3,16 @@ import { useFetchLinks } from '../../hooks'; // Adjust the import path as necess
 
 function SavedLinks() {
   const { links } = useFetchLinks();
+  const columns = [
+    { key: "filename", label: "Filename" },
+  ];
 
   return (
-    <div>
+    <>
       <h1>Saved Links</h1>
-      {/* console log links */}
-      {console.log(links)}
-      
-      {/* <Table
-        aria-label="Example table with static content"
-        css={{
-          height: "auto",
-          minWidth: "100%",
-        }}
-      >
-        <Table.Header>
-          <Table.Column>ID</Table.Column>
-          <Table.Column>Link</Table.Column>
-        </Table.Header>
-        <Table.Body>
-          {links.map((link, index) => (
-            <Table.Row key={index}>
-              <Table.Cell>{link.id}</Table.Cell>
-              <Table.Cell>{link.link}</Table.Cell>
-            </Table.Row>
-          ))}
-        </Table.Body>
-      </Table> */}
-    </div>
+      {/* TODO FIX DISPLAY */}
+      {/* {links && columns && <Table data={links} columns={columns} />} */}
+    </>
   );
 }
 
