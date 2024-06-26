b59256132d6fb77ee14b263e3ccc1cb7f24c9649
Crackvignoule <88386090+Crackvignoule@users.noreply.github.com>
2024-06-26 08:47:07 +0200

    WIP SavedLinks.jsx

--- client/src/pages/SavedLinks/SavedLinks.jsx
+++ client/src/pages/SavedLinks/SavedLinks.jsx
@@ -5,13 +5,46 @@ function SavedLinks() {
   const { links } = useFetchLinks();
   const columns = [
     { key: "filename", label: "Filename" },
+    { key: "actions", label: "Actions" },
   ];
 
   return (
     <>
-      <h1>Saved Links</h1>
-      {/* TODO FIX DISPLAY */}
-      {/* {links && columns && <Table data={links} columns={columns} />} */}
+      <Table aria-label="Debrid Results" className="text-cadet-grey">
+            <TableHeader columns={columns}>
+              {(column) => (
+                <TableColumn className="text-rich-black" key={column.key}>
+                  {column.label}
+                </TableColumn>
+              )}
+            </TableHeader>
+            <TableBody
+              items={debridResult.map((item, index) => ({
+                ...item,
+                no: index + 1,
+                action: "Save", // TODO useless ?
+              }))}
+            >
+              {(item) => (
+                <TableRow key={item.no}>
+                  {(columnKey) => (
+                    <TableCell>
+                      {columnKey === "actions" ? (
+                        <div className="flex items-center gap-2.5">
+                        </div>
+                      ) : columnKey === "filename" ? (
+                        <Tooltip content={item.filename} color="foreground" showArrow={true}>
+                          <span className="truncate max-w-sm block">{item.filename}</span>
+                        </Tooltip>
+                      ) : (
+                        item[columnKey]
+                      )}
+                    </TableCell>
+                  )}
+                </TableRow>
+              )}
+            </TableBody>
+          </Table>
     </>
   );
 }
