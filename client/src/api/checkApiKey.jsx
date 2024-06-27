335a68970c6d41bba4990188a6df15d3198a52fc
Crackvignoule <88386090+Crackvignoule@users.noreply.github.com>
2024-06-27 10:12:21 +0200

    Update checkApiKey.jsx

--- client/src/api/checkApiKey.jsx
+++ client/src/api/checkApiKey.jsx
@@ -3,7 +3,7 @@ import axios from "axios";
 const PrefixUrl = window.RUNTIME_CONFIG.URL_PREFIX;
 
 const checkApiKey = async (key) => {
-  const response = await axios.get(`${PrefixUrl}/api/checkApiKey/${key}`);
+  const response = await axios.get(`${PrefixUrl}/api/user/checkApiKey/${key}`);
   const res = response.data;
   if (!res.isValid) {
     throw new Error("API key is invalid");
