b3f74c14c53d95c05c9585acb0ae4a46cabf4506
Crackvignoule <kiki.kalagan@gmail.com>
2024-06-27 08:12:48 +0200

    adds search bar

--- client/src/pages/SavedLinks/SavedLinks.jsx
+++ client/src/pages/SavedLinks/SavedLinks.jsx
@@ -1,15 +1,16 @@
 // TODO Refactor
 import { useState } from 'react';
-import { Pagination } from "@nextui-org/react";
+import { Pagination, Input } from "@nextui-org/react";
 import { useLinkManagement } from '../../hooks';
 import { useNavigate } from 'react-router-dom';
 import { CommonTable, ActionButton } from '../../components';
-import { Download, Trash2 } from 'lucide-react';
+import { Download, Search, Trash2 } from 'lucide-react';
 
 function SavedLinks() {
   const navigate = useNavigate();
   const { links, deleteLinks } = useLinkManagement();
   const [currentPage, setCurrentPage] = useState(1);
+  const [searchQuery, setSearchQuery] = useState(""); // State to hold the search query
   const itemsPerPage = 10;
 
   const columns = [
@@ -17,9 +18,14 @@ function SavedLinks() {
     { key: "actions", label: "Actions" },
   ];
 
+  // Filter links based on the search query before slicing for pagination
+  const filteredLinks = links.filter(link => 
+    link.filename.toLowerCase().includes(searchQuery.toLowerCase())
+  );
+
   const indexOfLastItem = currentPage * itemsPerPage;
   const indexOfFirstItem = indexOfLastItem - itemsPerPage;
-  const currentItems = links.slice(indexOfFirstItem, indexOfLastItem);
+  const currentItems = filteredLinks.slice(indexOfFirstItem, indexOfLastItem);
 
   const handlePageChange = (page) => setCurrentPage(page);
 
@@ -27,8 +33,21 @@ function SavedLinks() {
     navigate('/', { state: { link } });
   };
 
+  // Update the search query state on input change
+  const handleSearchChange = (e) => {
+    setSearchQuery(e);
+    setCurrentPage(1); // Reset to the first page to show filtered results from the beginning
+  };
+
   return (
     <>
+       {/* TODO Change color of Logo and caret and text rich-black */}
+      <Input
+        placeholder="Search"
+        className="m-5 w-1/2 mx-auto"
+        startContent={<Search />}
+        onValueChange={handleSearchChange}
+      />
       <CommonTable
         columns={columns}
         items={currentItems.map((item, index) => ({
